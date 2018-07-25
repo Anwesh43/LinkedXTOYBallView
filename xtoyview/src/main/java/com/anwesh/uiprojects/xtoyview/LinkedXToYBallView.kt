acb8155f320bd8c2843ab016047ff50ade62624f
Anwesh43 <anweshthecool0@gmail.com>
2018-07-26 01:48:56 +0530

    adding Animator class

--- xtoyview/src/main/java/com/anwesh/uiprojects/xtoyview/LinkedXToYBallView.kt
+++ xtoyview/src/main/java/com/anwesh/uiprojects/xtoyview/LinkedXToYBallView.kt
@@ -49,4 +49,32 @@ class LinkedXToYBallView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch (ex : Exception) {
+
+                }
+            }
+        }
+    }
+
 }
