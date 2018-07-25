d133cbae4b11be6c41c7ba580de9266af4d86a72
Anwesh43 <anweshthecool0@gmail.com>
2018-07-26 01:41:40 +0530

    adding LinkedXTOYView class

--- xtoyview/src/main/java/com/anwesh/uiprojects/xtoyview/LinkedXToYBallView.kt
+++ xtoyview/src/main/java/com/anwesh/uiprojects/xtoyview/LinkedXToYBallView.kt
@@ -3,3 +3,29 @@ package com.anwesh.uiprojects.xtoyview
 /**
  * Created by anweshmishra on 26/07/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val NODES : Int = 5
+
+class LinkedXToYBallView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
