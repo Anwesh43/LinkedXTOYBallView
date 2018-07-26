34efa54d96d3c0f024246af6ef60e17c155312e3
Anwesh43 <anweshthecool0@gmail.com>
2018-07-26 10:15:16 +0530

    adding OnAnimationCompleteListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedxtoyballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedxtoyballview/MainActivity.kt
@@ -3,14 +3,28 @@ package com.anwesh.uiprojects.linkedxtoyballview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.xtoyview.LinkedXToYBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedXToYBallView.create(this)
+        val view : LinkedXToYBallView = LinkedXToYBallView.create(this)
         fullScreen()
+        view.addOnCompletionListener({i -> createCompleteToast(i)},{i -> createResetToast(i)})
+    }
+
+    private fun createCompleteToast(i : Int) {
+        createToast("animation number ${i} is completed")
+    }
+
+    private fun createResetToast(i : Int) {
+        createToast("animation number ${i} is reset")
+    }
+
+    private fun createToast(msg : String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
 
